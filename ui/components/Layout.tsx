bf582bfaa6d576f4c970475edfeab900f5ba894c
Dmytro <d-dmytro@users.noreply.github.com>
2019-07-27 14:23:14 +0300

    Adding the Layout and Common Styles

---
+++ ui/components/Layout.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Link from 'next/link';
+
+export const Layout: React.FunctionComponent = ({ children }) => {
+  return (
+    <div className="pageContainer">
+      <Link href="/">
+        <a className="logo">
+          <img src="/static/logo.png" />
+        </a>
+      </Link>
+      {children}
+      <style jsx global>{`
+        html {
+          box-sizing: border-box;
+        }
+        *,
+        *:before,
+        *:after {
+          box-sizing: inherit;
+        }
+        body {
+          color: #5d647b;
+          font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+          font-size: 18px;
+          line-height: 1.5;
+          margin: 0;
+        }
+        a {
+          text-decoration: none;
+        }
+      `}</style>
+      <style jsx>{`
+        .pageContainer {
+          margin: 0 auto;
+          max-width: 600px;
+          padding: 0 15px;
+        }
+        .logo {
+          display: flex;
+          justify-content: center;
+          margin: 30px 0;
+        }
+        img {
+          width: 150px;
+        }
+      `}</style>
+    </div>
+  );
+};
--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NextPage } from 'next';
 import { TasksComponent, TaskStatus } from '../generated/graphql';
+import { Layout } from '../components/Layout';
 
 interface InitialProps {
   greeting: string;
@@ -10,25 +11,27 @@ interface Props extends InitialProps {}
 
 const IndexPage: NextPage<Props, InitialProps> = props => {
   return (
-    <TasksComponent variables={{ status: TaskStatus.Active }}>
-      {({ loading, error, data }) => {
-        if (loading) {
-          return <p>Loading.</p>;
-        } else if (error) {
-          return <p>An error occurred.</p>;
-        }
+    <Layout>
+      <TasksComponent variables={{ status: TaskStatus.Active }}>
+        {({ loading, error, data }) => {
+          if (loading) {
+            return <p>Loading.</p>;
+          } else if (error) {
+            return <p>An error occurred.</p>;
+          }
 
-        const tasks = data && data.tasks ? data.tasks : [];
+          const tasks = data && data.tasks ? data.tasks : [];
 
-        return (
-          <ul>
-            {tasks.map(task => {
-              return <li key={task.id}>{task.title}</li>;
-            })}
-          </ul>
-        );
-      }}
-    </TasksComponent>
+          return (
+            <ul>
+              {tasks.map(task => {
+                return <li key={task.id}>{task.title}</li>;
+              })}
+            </ul>
+          );
+        }}
+      </TasksComponent>
+    </Layout>
   );
 };
 
